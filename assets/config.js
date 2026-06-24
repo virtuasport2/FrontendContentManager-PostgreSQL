7337ed5ae90169ab8812318519d44328c4e5b088
virtuasport2 <78851343+virtuasport2@users.noreply.github.com>
2026-06-24 10:36:03 +0200

    Toggle API_BASE_URL for production and local use

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // REMOTO
-   //API_BASE_URL: "https://api-content-manager-postgresql.onrender.com"
+   API_BASE_URL: "https://api-content-manager-postgresql.onrender.com"
   
   // Anche se sembrano uguali:
   // localhost e
   // 127.0.0.1
   // per il browser possono comportarsi in modo diverso (soprattutto con CORS, sessioni, cache e cookie).
-  API_BASE_URL: "http://localhost:8080"
+  //API_BASE_URL: "http://localhost:8080"
 };
