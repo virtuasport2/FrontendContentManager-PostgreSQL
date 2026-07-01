ae5b1b4209f6cddbd3bb0869dcd982cfee585af4
virtuasport2 <78851343+virtuasport2@users.noreply.github.com>
2026-07-01 08:32:55 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // REMOTO
   API_BASE_URL: "https://api-content-manager-postgresql.onrender.com"
-  //WS_URL:"https://api-content-manager-postgresql.onrender.com"
+  WS_URL:"https://api-content-manager-postgresql.onrender.com"
   // Anche se sembrano uguali:
   // localhost e
   // 127.0.0.1
